84362ea0914fa813c23567e9e3cc1625ab61a9e4
Jonathan Heye <jonheye82@gmail.com>
2019-01-22 23:30:15 -0600

    flipped middleware order

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const morgan = require("morgan");
 const bprouter = require("./blogPostsRouter");
 const app = express();
 
-app.use('/blog-posts', bprouter);
 app.use(morgan("common"));
 app.use(express.static('public'));
 app.use(express.json());
+app.use('/blog-posts', bprouter);
 
 let server;
 
