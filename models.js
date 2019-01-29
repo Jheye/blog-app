d5bc8a78b5e20af5b895b6990b78fac9a143f29d
Jonathan Heye <jonheye82@gmail.com>
2019-01-28 18:46:27 -0600

    added author schema and endpoints

--- models.js
+++ models.js
@@ -3,21 +3,34 @@
 const mongoose = require('mongoose');//require and add es6 promises like server.js
 mongoose.Promise = global.Promise;  //make mongoose use ES6 promises
 
+
+var authorSchema = mongoose.Schema({
+  firstName: 'string',
+  lastName: 'string',
+  userName: {
+    type: 'string',
+    unique: true
+  }
+});
+
+var commentSchema = mongoose.Schema({ content: 'string' });
+
 //Below model schema defines how every document in this collection should look
-const BlogPostSchema = mongoose.Schema({
-  author: {
-    firstName: String,
-    lastName: String,
-  },
-  title: {
-    type: String, 
-    required: true},
-  content: {
-    type: String,
-    required: true},
-  created: {
-    type: Date,
-     default: Date.now}
+var BlogPostSchema = mongoose.Schema({
+  title: 'string',
+  content: 'string',
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'Author' },
+  comments: [commentSchema]
+});
+
+blogPostSchema.pre('find', function(next) {
+  this.populate('author');
+  next();
+});
+
+blogPostSchema.pre('findOne', function(next) {
+  this.populate('author');
+  next();
 });
 
 //Using virtuals to create a property on the model
@@ -32,12 +45,16 @@ BlogPostSchema.methods.serialize = function() {
     author: this.authorName,
     content: this.content,
     title: this.title,
-    created: this.created
+    comments: this.comments
   };
 };
 
+
+//Added Author export
 //Now create the new mongoose model BlogPost that uses BlogPostSchema that is defined above
 //The model('BlogPost, ) argument determines the collection
+var Author = mongoose.model('Author', authorSchema);
+
 const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
 
-module.exports = { BlogPost };
+module.exports = { Author, BlogPost };
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const morgan = require("morgan");
 const mongoose = require('mongoose');//require
 mongoose.Promise = global.Promise;  //make mongoose use ES6 promises
 const { DATABASE_URL, PORT } = require('./config');//require config.js file for PORT/DATABASE_URL & future test url
-const { Blogpost } = require('./models');
+const { Author, BlogPost } = require('./models');
 const app = express();
 
 app.use(morgan("common"));
@@ -47,8 +47,8 @@ app.post('/posts', (req, res) => {
       const message = `Missing \`${field}\` in request body`;
       console.error(message);
       return res.status(400).send(message);
-    }
-  }
+    };
+  };
 
   BlogPost
     .create({
@@ -83,7 +83,7 @@ app.put('/posts/:id', (req, res) => {
     res.status(400).json({
       error: 'Request path id and request body id values must match'
     });
-  }
+  };
 
   const updated = {};
   const updateableFields = ['title', 'content', 'author'];
@@ -110,6 +110,107 @@ app.delete('/:id', (req, res) => {
 
 app.use('*', function (req, res) {
   res.status(404).json({ message: 'Not Found' });
+});
+
+//Add authors endpoints for POST, PUT, and DELETE
+app.post('/authors', (req, res) => {
+  const requiredFields = ['firstName', 'lastName', 'userName'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    };
+  };
+
+  Author
+    .findOne({ userName: req.body.userName })
+    .then(author => {
+      if (author) {
+        const message = `Username already taken`;
+        console.error(message);
+        return res.status(400).send(message);
+      }
+      else {
+        Author
+          .create({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            userName: req.body.userName
+          })
+          .then(author => res.status(201).json({
+              _id: author.id,
+              name: `${author.firstName} ${author.lastName}`,
+              userName: author.userName
+            }))
+          .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: 'Something went wrong' });
+          });
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'something went horribly awry' });
+    });
+});
+
+
+app.put('/authors/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    res.status(400).json({
+      error: 'Request path id and request body id values must match'
+    });
+  };
+
+  const updated = {};
+  const updateableFields = ['firstName', 'lastName', 'userName'];
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      updated[field] = req.body[field];
+    }
+  });
+
+  Author
+    .findOne({ userName: updated.userName || '', _id: { $ne: req.params.id } })
+    .then(author => {
+      if(author) {
+        const message = `Username already taken`;
+        console.error(message);
+        return res.status(400).send(message);
+      }
+      else {
+        Author
+          .findByIdAndUpdate(req.params.id, { $set: updated }, { new: true })
+          .then(updatedAuthor => {
+            res.status(200).json({
+              id: updatedAuthor.id,
+              name: `${updatedAuthor.firstName} ${updatedAuthor.lastName}`,
+              userName: updatedAuthor.userName
+            });
+          })
+          .catch(err => res.status(500).json({ message: err }));
+      }
+    });
+});
+
+
+app.delete('/authors/:id', (req, res) => {
+  BlogPost
+    .remove({ author: req.params.id })
+    .then(() => {
+      Author
+        .findByIdAndRemove(req.params.id)
+        .then(() => {
+          console.log(`Deleted blog posts owned by and author with id \`${req.params.id}\``);
+          res.status(204).json({ message: 'success' });
+        });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'something went terribly wrong' });
+    });
 });
 
 // closeServer needs access to a server object, but that only
